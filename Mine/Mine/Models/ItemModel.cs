4abe8f7dc6a178db497597716926e16bc2b99888
Peter Weisdepp <weisdepp@seattleu.edu>
2020-01-30 15:23:33 -0800

    Added Update Method to ItemModel

--- Mine/Mine/Models/ItemModel.cs
+++ Mine/Mine/Models/ItemModel.cs
@@ -9,5 +9,15 @@
 
         // The value of the item
         public int Value { get; set; } = 0;
+
+        public bool Update(ItemModel data)
+        {
+            Name = data.Name;
+            Description = data.Description;
+
+            Value = data.Value;
+
+            return true;
+        }
     }
 }
