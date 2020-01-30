4e4e66fdf226a6c4203d8d743cbc637f22ac10e1
Peter Weisdepp <weisdepp@seattleu.edu>
2020-01-29 22:29:30 -0800

    Added Read Method to ItemIndexViewModel

--- Mine/Mine/ViewModels/ItemIndexViewModel.cs
+++ Mine/Mine/ViewModels/ItemIndexViewModel.cs
@@ -61,6 +61,11 @@ namespace Mine.ViewModels
             return true;
         }
 
+        public async Task<ItemModel> Read(String id)
+        {
+            var result = await DataStore.ReadAsync(id);
+            return result;
+        }
         #region Refresh
         // Return True if a refresh is needed
         // It sets the refresh flag to false
