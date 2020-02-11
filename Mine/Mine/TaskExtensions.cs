e4e82bd1b5880d02718665d2681b48367b522074
Peter Weisdepp <weisdepp@seattleu.edu>
2020-02-11 12:35:52 -0800

    Added TaskExtension

---
+++ Mine/Mine/TaskExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Mine
+{
+    public static class TaskExtensions
+    {
+        public static async void SafeFireAndForget(this Task task, 
+            bool returnToCallingContext,
+            Action<Exception> onException = null)
+        {
+            try
+            {
+                await task.ConfigureAwait(returnToCallingContext);
+            }
+            catch (Exception e) when (onException != null)
+            {
+                onException(e);
+            }
+        }
+    }
+}
